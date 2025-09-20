4e60d182ff8b917aa25d161843b65859b0709d8b
daniil <danilka.radaev.00@mail.ru>
2025-09-20 23:53:02 +0300

    закончен, протестирован ContentProcessorService

--- ContentProcessor/src/main/java/com/adapter/contentprocessor/entity/Estate.java
+++ ContentProcessor/src/main/java/com/adapter/contentprocessor/entity/Estate.java
@@ -2,12 +2,20 @@ package com.adapter.contentprocessor.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString//подправить вывод
+@EqualsAndHashCode
 public class Estate {
 
     @Id
@@ -18,20 +26,22 @@ public class Estate {
             regexp = "^(\\d{2}:\\d{2}:\\d{6,7}:\\d{1,4})$",
             message = "Неверный формат кадастрового номера. Ожидаемый формат: XX:XX:XXXXXXX:XXX"
     )
+    @Column(unique = true, nullable = false)
     private String cadastr;
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     private Type type;
 
 
     @NotNull(message = "Укажите площадь")
     private String square;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "estate_price", joinColumns = @JoinColumn(name = "estate_id"))
     @MapKeyColumn(name = "price_key")
     @Column(name = "price_value")
-    private HashMap<String, BigDecimal> price;
+    private Map<String, BigDecimal> price = new HashMap<>();
 
 
     public enum Type {
--- ContentProcessor/src/main/java/com/adapter/contentprocessor/repository/EstateRepository.java
+++ ContentProcessor/src/main/java/com/adapter/contentprocessor/repository/EstateRepository.java
@@ -3,7 +3,13 @@ package com.adapter.contentprocessor.repository;
 import com.adapter.contentprocessor.entity.Estate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EstateRepository extends JpaRepository<Estate, UUID> {
+    Optional<Estate> findByCadastr(String cadastr);
+
+    List<Estate> findAllByCadastrIn(Collection<String> cadastrList);
 }
